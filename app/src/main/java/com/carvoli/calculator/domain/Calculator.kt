27e50fd28d43e3b1c5b969a620dbaaebd50c756b
João Victor <joaocarvoli@hotmail.com>
2023-03-16 20:21:08 -0300

    Creating the basic methods: sum, sub, mul and div

--- app/src/main/java/com/carvoli/calculator/domain/Calculator.kt
+++ app/src/main/java/com/carvoli/calculator/domain/Calculator.kt
@@ -1,19 +1,22 @@
 package com.carvoli.calculator.domain
 
 class Calculator {
-    fun add(){
-
+    fun add(a : Double, b : Double) : Double {
+        return a.plus(b)
     }
 
-    fun sub(){
-
+    fun sub(a : Double, b : Double) : Double {
+        return a.minus(b)
     }
 
-    fun mul(){
-
+    fun mul(a : Double, b : Double) : Double {
+        return a.times(b)
     }
 
-    fun div(){
-
+    fun div(a : Double, b : Double) : Double {
+        if(b.toInt() != 0){
+            return a.div(b)
+        }
+        return 0.0
     }
 }
