579dcf1dd794b3c104ccc883ec1b031876bc6d4d
João Victor <joaocarvoli@hotmail.com>
2023-03-19 12:10:00 -0300

    Testing ViewModel

--- app/src/main/java/com/carvoli/calculator/MainActivity.kt
+++ app/src/main/java/com/carvoli/calculator/MainActivity.kt
@@ -6,26 +6,33 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.view.isVisible
-import com.carvoli.calculator.data.CalculatorObserver
-import com.carvoli.calculator.domain.Operations
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
+import androidx.viewbinding.ViewBindings
+import com.carvoli.calculator.ui.CalculatorViewModel
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
+    private lateinit var calculatorViewModel: CalculatorViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calculatorViewModel = ViewModelProvider(this)[CalculatorViewModel::class.java]
     }
 
 
     fun getDigit(view : View){
         val digit = findViewById<Button>(view.id)
         updateTextViewCalculus(digit.text)
+        calculatorViewModel.onNumberButton(digit.text.toString())
     }
 
     fun getOperator(view : View){
         val operator = findViewById<Button>(view.id)
         updateTextViewCalculus(operator.text)
+        calculatorViewModel.onOperatorButton(operator.text.toString())
     }
 
     fun removeDigit(view : View?){
